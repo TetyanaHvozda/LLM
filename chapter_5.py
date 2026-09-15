2177a452779147b8d203dc6b19ba4b8b8df2bc81
Tetyana Hvozda <tetyanahvozda@gmail.com>
2026-09-15 19:29:45 -0400

    added temperature scaling and top -k techniques

--- chapter_5.py
+++ chapter_5.py
@@ -318,3 +318,92 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
 
 epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
 plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+# Temperature scaling technique - adds a probabilistic selection
+#process to the next-token generation task.
+# torch.multinomial instead of torch.argmax()
+# next_token_id = torch.multinomial(probas, num_samples=1).item()
+vocab = { 
+    "closer": 0,
+    "every": 1, 
+    "effort": 2, 
+    "forward": 3,
+    "inches": 4,
+    "moves": 5, 
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+} 
+
+inverse_vocab = {v: k for k, v in vocab.items()}
+
+# Suppose input is "every effort moves you", and the LLM
+# returns the following logits for the next token:
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+
+# The next generated token is then as follows:
+print(inverse_vocab[next_token_id])
+
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123) # Manual seed for reproducibility
+    sample = [torch.multinomial(probas, num_samples=1).item() for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample), minlength=len(probas))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+print_sampled_tokens(probas)
+# Temperature scaling is just a fancy description for dividing the logits
+#by a number greater than 0
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+# Temperature values
+temperatures = [1, 0.1, 5]  # Original, higher confidence, and lower confidence
+
+# Calculate scaled probabilities
+scaled_probas = [softmax_with_temperature(next_token_logits, T) for T in temperatures]
+
+# Plotting
+# x = torch.arange(len(vocab))
+# bar_width = 0.15
+
+# fig, ax = plt.subplots(figsize=(5, 3))
+# for i, T in enumerate(temperatures):
+#     rects = ax.bar(x + i * bar_width, scaled_probas[i], bar_width, label=f'Temperature = {T}')
+
+# ax.set_ylabel('Probability')
+# ax.set_xticks(x)
+# ax.set_xticklabels(vocab.keys(), rotation=90)
+# ax.legend()
+
+# plt.tight_layout()
+# plt.savefig("temperature-plot.pdf")
+# plt.show()
+
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print("Top logits:", top_logits)
+print("Top positions:", top_pos)
+
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float("-inf")), 
+    other=next_token_logits
+)
+
+print(new_logits)
+
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
+
+###### text generation function #######
