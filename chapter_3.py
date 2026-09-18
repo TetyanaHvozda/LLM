bcda0e557131f9cc6df12222351209c7e203ee4d
Tetyana Hvozda <tetyanahvozda@gmail.com>
2026-09-18 16:09:27 -0400

    updated chapter 3 file

--- chapter_3.py
+++ chapter_3.py
@@ -4,106 +4,6 @@
 # 1. dot product of the query with every other input token
 import torch
 
-inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55]] # step     (x^6)
-)
-
-query = inputs[1]  # 2nd input token is the query (journey)
-
-attn_scores_2 = torch.empty(inputs.shape[0]) # 6 rows/words
-for i, x_i in enumerate(inputs):
-    attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
-
-print(attn_scores_2)
-
-# dot product is a measure of similarity
-# because it quantifies how closely two vectors are aligned: a higher dot product indi-
-# cates a greater degree of alignment or similarity between the vectors.
-
-# 2. Normalize each attention score
-attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
-
-print("Attention weights:", attn_weights_2_tmp)
-print("Sum:", attn_weights_2_tmp.sum())
-
-# Softmax normalization
-attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
-
-print("Attention weights:", attn_weights_2)
-print("Sum:", attn_weights_2.sum())
-
-# 3. context vector = sum(embedded input tokens * attention weight)
-query = inputs[1] # 2nd input token is the query
-
-context_vec_2 = torch.zeros(query.shape)
-for i,x_i in enumerate(inputs):
-    context_vec_2 += attn_weights_2[i]*x_i
-
-print(context_vec_2)
-
-# simple self-attention mechanism without trainable weights for all input tokens (words)
-# 1. compute attention scores
-attn_scores = inputs @ inputs.T
-print(attn_scores)
-
-# 2. compute attention weights
-attn_weights = torch.softmax(attn_scores, dim=-1)# -1 to normalize across rows (values from each column)
-print(attn_weights)
-
-# 3. compute context vectors
-all_context_vecs = attn_weights @ inputs
-print(all_context_vecs)
-
-#### A self-attention mechanism with trainable weights ####
-# Maybe I will use this method
-# scaled dot product attention
-x_2 = inputs[1] # second input element
-d_in = inputs.shape[1] # the input embedding size, d=3
-d_out = 2 # the output embedding size, d=2
-
-# 1. Compute 3 weight parameters (matrices)
-torch.manual_seed(123)
-
-W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-
-query_2 = x_2 @ W_query # _2 because it's with respect to the 2nd input element
-key_2 = x_2 @ W_key 
-value_2 = x_2 @ W_value
-
-print(query_2)
-
-# key and values for all inputs
-keys = inputs @ W_key 
-values = inputs @ W_value
-
-print("keys.shape:", keys.shape)
-print("values.shape:", values.shape)
-
-# 2. compute attention scores
-keys_2 = keys[1] # Python starts index at 0
-attn_score_22 = query_2.dot(keys_2)
-print(attn_score_22)
-
-attn_scores_2 = query_2 @ keys.T # All attention scores for given query
-print(attn_scores_2)
-
-# 3. compute attention weights
-d_k = keys.shape[1]
-attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1) # scale to improve the
-#training performance by avoiding small gradients
-print(attn_weights_2)
-
-# 4. compute context vectors
-context_vec_2 = attn_weights_2 @ values
-print(context_vec_2)
-
 # self-attention Python class
 import torch.nn as nn
 
@@ -128,9 +28,6 @@ class SelfAttention_v1(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
 
-torch.manual_seed(123)
-sa_v1 = SelfAttention_v1(d_in, d_out)
-print(sa_v1(inputs))
 
 # class with nn.Linear; has an optimized weight initialization scheme
 class SelfAttention_v2(nn.Module):
@@ -151,62 +48,6 @@ class SelfAttention_v2(nn.Module):
 
         context_vec = attn_weights @ values
         return context_vec
-
-torch.manual_seed(789)
-sa_v2 = SelfAttention_v2(d_in, d_out)
-print(sa_v2(inputs))
-
-#### Causal attention ####
-# It restricts a model to only consider previous and current inputs in a sequence
-#when processing any given token when computing attention scores. 
-# it is a diagonal, all 0 on the right side
-queries = sa_v2.W_query(inputs)
-keys = sa_v2.W_key(inputs) 
-attn_scores = queries @ keys.T
-
-# 1. compute the attention weights using the softmax function
-attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
-print(attn_weights)
-
-# 2. create a mask where the values above the diagonal are zero
-context_length = attn_scores.shape[0]
-mask_simple = torch.tril(torch.ones(context_length, context_length))
-print(mask_simple)
-
-# 3. multiply this mask with the attention weights to zero-out the values above
-#the diagonal
-masked_simple = attn_weights*mask_simple
-print(masked_simple)
-
-# 4. renormalize the attention weights to sum up to 1 again in each row
-row_sums = masked_simple.sum(dim=-1, keepdim=True)
-masked_simple_norm = masked_simple / row_sums
-print(masked_simple_norm)
-
-# attention scores (unnormalized) => masked attention scores (unnormalized) => masked attention weights
-mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
-masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
-print(masked)
-
-attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
-print(attn_weights)
-
-# dropout - prevents overfitting by droping out rndomly selected hidden layer units.
-# apply after computing the attention weights
-torch.manual_seed(123)
-dropout = torch.nn.Dropout(0.5) # dropout rate of 50%
-example = torch.ones(6, 6) # create a matrix of ones
-
-# elements in the matrix are scaled: 1/0.5 = 2
-print(dropout(example))
-
-# apply to attention weights
-torch.manual_seed(123)
-print(dropout(attn_weights))
-
-# Causeal attention class
-batch = torch.stack((inputs, inputs), dim=0)
-print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
 
 class CausalAttention(nn.Module):
 
@@ -241,16 +82,6 @@ class CausalAttention(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
 
-torch.manual_seed(123)
-
-context_length = batch.shape[1]
-ca = CausalAttention(d_in, d_out, context_length, 0.0)
-
-context_vecs = ca(batch)
-
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
-
 # multi-head attention
 class MultiHeadAttentionWrapper(nn.Module):
 
@@ -263,20 +94,6 @@ class MultiHeadAttentionWrapper(nn.Module):
 
     def forward(self, x):
         return torch.cat([head(x) for head in self.heads], dim=-1)
-
-
-torch.manual_seed(123)
-
-context_length = batch.shape[1] # This is the number of tokens
-d_in, d_out = 3, 2
-mha = MultiHeadAttentionWrapper(
-    d_in, d_out, context_length, 0.0, num_heads=2
-)
-
-context_vecs = mha(batch)
-
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
 
 # Implementing multi-head attention with weight splits
 # efficient multi-head attention class
@@ -344,13 +161,198 @@ class MultiHeadAttention(nn.Module):
 
         return context_vec
 
-torch.manual_seed(123)
 
-batch_size, context_length, d_in = batch.shape
-d_out = 2
-mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+if __name__ == "__main__":
+    inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], # Your     (x^1)
+    [0.55, 0.87, 0.66], # journey  (x^2)
+    [0.57, 0.85, 0.64], # starts   (x^3)
+    [0.22, 0.58, 0.33], # with     (x^4)
+    [0.77, 0.25, 0.10], # one      (x^5)
+    [0.05, 0.80, 0.55]] # step     (x^6)
+    )
 
-context_vecs = mha(batch)
+    query = inputs[1]  # 2nd input token is the query (journey)
 
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
+    attn_scores_2 = torch.empty(inputs.shape[0]) # 6 rows/words
+    for i, x_i in enumerate(inputs):
+        attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
+
+    print(attn_scores_2)
+
+    # dot product is a measure of similarity
+    # because it quantifies how closely two vectors are aligned: a higher dot product indi-
+    # cates a greater degree of alignment or similarity between the vectors.
+
+    # 2. Normalize each attention score
+    attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
+
+    print("Attention weights:", attn_weights_2_tmp)
+    print("Sum:", attn_weights_2_tmp.sum())
+
+    # Softmax normalization
+    attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
+
+    print("Attention weights:", attn_weights_2)
+    print("Sum:", attn_weights_2.sum())
+
+    # 3. context vector = sum(embedded input tokens * attention weight)
+    query = inputs[1] # 2nd input token is the query
+
+    context_vec_2 = torch.zeros(query.shape)
+    for i,x_i in enumerate(inputs):
+        context_vec_2 += attn_weights_2[i]*x_i
+
+    print(context_vec_2)
+
+    # simple self-attention mechanism without trainable weights for all input tokens (words)
+    # 1. compute attention scores
+    attn_scores = inputs @ inputs.T
+    print(attn_scores)
+
+    # 2. compute attention weights
+    attn_weights = torch.softmax(attn_scores, dim=-1)# -1 to normalize across rows (values from each column)
+    print(attn_weights)
+
+    # 3. compute context vectors
+    all_context_vecs = attn_weights @ inputs
+    print(all_context_vecs)
+
+    #### A self-attention mechanism with trainable weights ####
+    # Maybe I will use this method
+    # scaled dot product attention
+    x_2 = inputs[1] # second input element
+    d_in = inputs.shape[1] # the input embedding size, d=3
+    d_out = 2 # the output embedding size, d=2
+
+    # 1. Compute 3 weight parameters (matrices)
+    torch.manual_seed(123)
+
+    W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+    W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+    W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+    query_2 = x_2 @ W_query # _2 because it's with respect to the 2nd input element
+    key_2 = x_2 @ W_key 
+    value_2 = x_2 @ W_value
+
+    print(query_2)
+
+    # key and values for all inputs
+    keys = inputs @ W_key 
+    values = inputs @ W_value
+
+    print("keys.shape:", keys.shape)
+    print("values.shape:", values.shape)
+
+    # 2. compute attention scores
+    keys_2 = keys[1] # Python starts index at 0
+    attn_score_22 = query_2.dot(keys_2)
+    print(attn_score_22)
+
+    attn_scores_2 = query_2 @ keys.T # All attention scores for given query
+    print(attn_scores_2)
+
+    # 3. compute attention weights
+    d_k = keys.shape[1]
+    attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1) # scale to improve the
+    #training performance by avoiding small gradients
+    print(attn_weights_2)
+
+    # 4. compute context vectors
+    context_vec_2 = attn_weights_2 @ values
+    print(context_vec_2)
+
+    torch.manual_seed(123)
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    print(sa_v1(inputs))
+
+    torch.manual_seed(789)
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    print(sa_v2(inputs))
+
+    #### Causal attention ####
+    # It restricts a model to only consider previous and current inputs in a sequence
+    #when processing any given token when computing attention scores. 
+    # it is a diagonal, all 0 on the right side
+    queries = sa_v2.W_query(inputs)
+    keys = sa_v2.W_key(inputs) 
+    attn_scores = queries @ keys.T
+
+    # 1. compute the attention weights using the softmax function
+    attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+    print(attn_weights)
+
+    # 2. create a mask where the values above the diagonal are zero
+    context_length = attn_scores.shape[0]
+    mask_simple = torch.tril(torch.ones(context_length, context_length))
+    print(mask_simple)
+
+    # 3. multiply this mask with the attention weights to zero-out the values above
+    #the diagonal
+    masked_simple = attn_weights*mask_simple
+    print(masked_simple)
+
+    # 4. renormalize the attention weights to sum up to 1 again in each row
+    row_sums = masked_simple.sum(dim=-1, keepdim=True)
+    masked_simple_norm = masked_simple / row_sums
+    print(masked_simple_norm)
+
+    # attention scores (unnormalized) => masked attention scores (unnormalized) => masked attention weights
+    mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+    masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+    print(masked)
+
+    attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+    print(attn_weights)
+
+    # dropout - prevents overfitting by droping out rndomly selected hidden layer units.
+    # apply after computing the attention weights
+    torch.manual_seed(123)
+    dropout = torch.nn.Dropout(0.5) # dropout rate of 50%
+    example = torch.ones(6, 6) # create a matrix of ones
+
+    # elements in the matrix are scaled: 1/0.5 = 2
+    print(dropout(example))
+
+    # apply to attention weights
+    torch.manual_seed(123)
+    print(dropout(attn_weights))
+
+    # Causeal attention class
+    batch = torch.stack((inputs, inputs), dim=0)
+    print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+    
+    torch.manual_seed(123)
+
+    context_length = batch.shape[1]
+    ca = CausalAttention(d_in, d_out, context_length, 0.0)
+
+    context_vecs = ca(batch)
+
+    print(context_vecs)
+    print("context_vecs.shape:", context_vecs.shape)
+
+    torch.manual_seed(123)
+
+    context_length = batch.shape[1] # This is the number of tokens
+    d_in, d_out = 3, 2
+    mha = MultiHeadAttentionWrapper(
+        d_in, d_out, context_length, 0.0, num_heads=2
+    )
+
+    context_vecs = mha(batch)
+
+    print(context_vecs)
+    print("context_vecs.shape:", context_vecs.shape)
+
+    torch.manual_seed(123)
+
+    batch_size, context_length, d_in = batch.shape
+    d_out = 2
+    mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+
+    context_vecs = mha(batch)
+
+    print(context_vecs)
+    print("context_vecs.shape:", context_vecs.shape)
